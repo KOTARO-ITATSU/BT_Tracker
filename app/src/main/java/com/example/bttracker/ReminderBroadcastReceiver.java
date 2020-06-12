602d0491ba35cadff480dbfb4c3f9a9902af6929
KOTARO-ITATSU <2020m098@kuas.ac.jp>
2020-06-12 20:31:25 +0900

    "Homework 5.3: extended ReminderBroadcastReceiver to start LogActivity directly from notification"

--- app/src/main/java/com/example/bttracker/ReminderBroadcastReceiver.java
+++ app/src/main/java/com/example/bttracker/ReminderBroadcastReceiver.java
@@ -1,5 +1,8 @@
 package com.example.bttracker;
 
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,17 +12,40 @@ import androidx.core.app.NotificationManagerCompat;
 
 public class ReminderBroadcastReceiver extends BroadcastReceiver {
 
+
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        Intent notificationIntent = new Intent(context, LogActivity.class);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(context,
+                0, notificationIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationManager nm = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "BT_Tracker_Channel")
                 .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setContentTitle("Notification from BT Tracker")
                 .setContentText("Please log your body temperature now")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(contentIntent);
+
         NotificationManagerCompat notificationManager =
-         NotificationManagerCompat.from(context);
+                NotificationManagerCompat.from(context);
         notificationManager.notify(200, builder.build());
+
+
+
+
+
+
+
+
+
     }
-}
+    }
